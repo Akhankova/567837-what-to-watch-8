e9503eb05dee5172de395b0d4b2cf41003153658
Alina Akhankova <a.akhankova@gmail.com>
2021-10-03 13:49:11 +0200

    Создаю React-компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -3,10 +3,12 @@ import WelcomeScreen from '../welcome-screen/welcome-screen';
 type AppScreenProps = {
   year: number;
   genre: string;
+  cardsCount: number;
+  headCardTitle: string;
 }
 
-function App({year, genre}: AppScreenProps): JSX.Element {
-  return (<WelcomeScreen year={year} genre={genre}/>);
+function App({year, genre, cardsCount, headCardTitle}: AppScreenProps): JSX.Element {
+  return (<WelcomeScreen year={year} genre={genre} cardsCount={cardsCount} headCardTitle={headCardTitle} />);
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 const Setting = {
+  CARDS_COUNT: 20,
   GENRE: 'Drama',
   YEAR: 2014,
+  HEAD_CARD_TITLE: 'The Grand Budapest Hotel',
 };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App genre = {Setting.GENRE} year = {Setting.YEAR}/>
+    <App genre = {Setting.GENRE} year = {Setting.YEAR} cardsCount={Setting.CARDS_COUNT} headCardTitle = {Setting.HEAD_CARD_TITLE}/>
   </React.StrictMode>,
   document.getElementById('root'));
